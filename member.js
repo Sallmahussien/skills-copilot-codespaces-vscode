2389d128337233a027e54dab4d95c4fa9891a877
Salma Hussien <salmaa.hussien1@gmail.com>
2024-06-12 12:57:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var member = {};
+    member.firstName = "Sara";
+    member.lastName = "Smith";
+    member.skills = ["JavaScript", "HTML", "CSS"];
+    return member;
+}
